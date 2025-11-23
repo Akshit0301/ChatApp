3ef4d654117957499dba133cfbe7ddc9966fd3e9
Akshit <akshit@Akshits-MacBook-Air.local>
2025-11-23 17:56:22 +0530

    changed cors url

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,8 +17,8 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "*", // or your frontend domain once deployed
-    credentials: true
+  origin: "https://chat-app-mu-gilt.vercel.app/", // Vercel frontend URL
+  credentials: true,
 }));
 
 
