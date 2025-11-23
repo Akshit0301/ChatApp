84a00d162391caf6bbd80506ab2a3940fb89e01c
Akshit <akshit@Akshits-MacBook-Air.local>
2025-11-23 17:36:47 +0530

    added backend url

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,9 +17,10 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: "*", // or your frontend domain once deployed
+    credentials: true
 }));
+
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
