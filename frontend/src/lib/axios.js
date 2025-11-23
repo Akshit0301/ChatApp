4f357d3dc670ce0dd8cfc37b176276d508f60067
Akshit <akshit@Akshits-MacBook-Air.local>
2025-11-23 18:11:10 +0530

    updated .env

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-    withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
