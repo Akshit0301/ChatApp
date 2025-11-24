a717f4f94042c5327dd41b254c5e644393b27fec
Akshit <akshit@Akshits-MacBook-Air.local>
2025-11-24 22:46:39 +0530

    added

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { connectDB } from "./lib/db.js";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
+const __dirname = path.resolve();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -38,6 +40,13 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("🚀 Server running on PORT: " + PORT);
